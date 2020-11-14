daeae0f2be34a0560b303a3207548d78d9c0c408
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-14 15:06:53 +0100

    design

---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import { InfoSection, Pricing } from '../../components';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjTwo} />
+      <Pricing />
+      <InfoSection {...homeObjFour} />
+    </React.Fragment>
+  );
+}
+
+export default Home;
